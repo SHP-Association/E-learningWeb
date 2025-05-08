3927c15ec0691618f7c9e6206567b5b422f6bd9d
sandesh patel <patelbr5118s@gmail.com>
2025-05-09 01:06:14 +0530

    add mail service

--- X/urls.py
+++ X/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from courses import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -14,4 +15,19 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('enroll/<slug:slug>/', views.enroll, name='enroll'),
     path('admin/', admin.site.urls),
+    # Password reset URLs
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='emails/password_reset_form.html',
+        email_template_name='emails/password_reset_email.html',
+        subject_template_name='emails/password_reset_subject.txt'
+    ), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='emails/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='emails/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'
+    ), name='emails/password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
