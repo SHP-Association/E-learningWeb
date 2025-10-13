aad246bb1cacc632319dfe91fc0c8ac248dce3dc
sandesh patel <patelbr5118s@gmail.com>
2025-10-13 21:50:13 +0530

    .

--- backend/X/settings.py
+++ backend/X/settings.py
@@ -67,17 +67,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = os.getenv('DJANGO_WSGI_APPLICATION')
-
-# --- Database Configuration ---
-DATABASE_ENGINE = os.getenv('DJANGO_DB_ENGINE')
-DATABASE_NAME = os.getenv('DJANGO_DB_NAME')
-
 DATABASES = {
     'default': {
-        'ENGINE': DATABASE_ENGINE,
-        'NAME': DATABASE_NAME,
+        'ENGINE': os.getenv('DJANGO_DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DJANGO_DB_NAME'),
+        'USER': os.getenv('DJANGO_DB_USER'),
+        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD'),
+        'HOST': os.getenv('DJANGO_DB_HOST', 'localhost'),
+        'PORT': os.getenv('DJANGO_DB_PORT', '5432'),
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
